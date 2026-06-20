312a07f376f3530643125348771e75cb9c456a1e
austell <joelifechukwu9@gmail.com>
2026-06-20 11:56:52 +0100

    Refactor: Restructured project to clean architecture (moved existing files)

--- fix_name.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('messages.db')
-cursor = conn.cursor()
-cursor.execute("DELETE FROM customers WHERE name = 'yes pls two'")
-conn.commit()
-print(f"Deleted {cursor.rowcount} record(s)")
-conn.close()
